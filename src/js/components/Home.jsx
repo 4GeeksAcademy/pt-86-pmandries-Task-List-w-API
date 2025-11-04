6a83f7dfd9c539852eb289704d281ba7c44bd7fd
pmandries <pandries@aol.com>
2025-11-04 19:30:52 +0000

    adds user, user data

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 
 //include images into your bundle
@@ -9,16 +9,45 @@ const Home = () => {
 	
 	const [tasks, setTasks] = useState([]);
 	const [userInput, setUserInput] = useState("");
-	const addToList = (task) => {
-		let newTask = {label: task, isDone: false}
-			setTasks([...tasks, newTask]);
+
+	useEffect(() => {
+		getUserData()
+	}, []); // This should only run once w/ empty array
+
+	async function getUserData () {
+		let response = await fetch("https://playground.4geeks.com/todo/users/pmandries");
+		let data = await response.json();
+			if (data.detail?.includes("doesn't exist")) {
+				createUser()
+			} else {
+				setTasks(data.todos);
+			}
+	}
+
+	async function createUser () {
+		let postResponse = await fetch("https://playground.4geeks.com/todo/users/pmandries", {
+			method: "POST",
+			headers: {"Content-type": "application/json"}
+		})
+		let postData = await postResponse.json()
+	}
+	
+	const addToList = async (task) => {
+		let newTask = {label: task, is_done: false}
+		let response = await fetch("https://playground.4geeks.com/todo/todos/pmandries", {
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify(newTask)
+		});
+		let data = await response.json()
+			getUserData();
 			setUserInput("")
 		};
 
 	const markAsDone = (index) => {
 		let updatedList = tasks.map((task, i) => {
 			if (i == index) {
-				return {label: task.label, isDone: !task.isDone}
+				return {label: task.label, is_done: !task.is_done}
 			}
 			else return task;
 		})
@@ -28,7 +57,7 @@ const Home = () => {
 	const deleteTask = (index) => {
 		let updatedList = tasks.filter((task, i) => {
 			if (i !== index) {
-				return {label: task.label, isDone: task.isDone}
+				return {label: task.label, is_done: task.is_done}
 			}
 		})
 		setTasks(updatedList)
@@ -68,7 +97,7 @@ const Home = () => {
 						/>
   						<label 
 							className = {
-								task.isDone ? "crossedOut form-check-label": "form-check-label" 
+								task.is_done ? "crossedOut form-check-label": "form-check-label" 
 							}
 							htmlFor="checkDefault">
     						{task.label}
@@ -79,6 +108,9 @@ const Home = () => {
 					</div>
 					)
 				}
+			</div>
+			<div className = "text-center">
+				<button className = "mt-5">Delete All</button>
 			</div>
 		</div>
 	);
